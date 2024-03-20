c618796218789972c53f21bd301a771e0a792163
Tomáš Petit <102812292+xpetitt123@users.noreply.github.com>
2024-03-20 15:40:36 +0100

    Adding the shell.nix file for the project
    Added a shell.nix file for further reproducibility

---
+++ Ceitec.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz") {} }:
+
+with pkgs;
+
+let
+  my-rpackages = rWrapper.override {
+    packages = with rPackages; [
+        R
+        ggplot2 
+        dplyr 
+        tidyverse 
+        tidymodels 
+        janitor
+        fda];
+  };
+
+  my-rstudio = rstudioWrapper.override {
+    packages = with rPackages; [
+        R 
+        dplyr 
+        tidyverse 
+        tidymodels 
+        fda
+        janitor
+        ggplot2
+    ];
+    };
+in
+mkShell {
+  buildInputs = [my-rpackages my-rstudio];
+  shellHook = ''
+      echo "Welcome to the Ceitec project shell!"
+    '';
+}
